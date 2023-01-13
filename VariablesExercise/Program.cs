48573c44fe370189237c16bd004a391eab76e162
Celeste McCall <celestemccall93@gmail.com>
2023-01-13 11:32:34 -0600

    Initial Commits

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String dogName; //Declartion syntax
+
+            dogName = "Jordan"; //Initilaized --- assigning a value
+
+            int dogAge = 5; //Declaring and initializing
+
+            char firstInitial = 'J'; //Declaring and initializing
+
+            bool isHungry = true; //Declaring and initializing
+
+            double dogWeight = 10.7; //Declaring and initializing
+
+            decimal dogHeight = 7.5m; //Declaring and initializing
+
+            Console.WriteLine($"My dog's name start with the initial {firstInitial}");
+            Console.WriteLine($"His name is {dogName} and he is {dogAge} years old.");
+            Console.WriteLine($"He weighs {dogWeight} pounds and he is {dogHeight} inches tall");
+            Console.WriteLine($"It is {isHungry} he is always hungry.");
+
+
+
+
+
         }
     }
 }
